def6070c35d6334659391cea50ad1c20200715e3
Anantha Morampudi <anantha.morampudi@ibm.com>
2022-11-03 10:01:40 +0530

    Adding the Selenium Automation code

---
+++ src/test/java/utility/ConfigRead.java
@@ -0,0 +1,33 @@
+package utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class ConfigRead {
+
+	Properties prop;
+
+	public ConfigRead() throws Exception {
+
+		FileInputStream fis = new FileInputStream("./testData/config.properties");
+
+		prop = new Properties();
+
+		prop.load(fis);
+
+	}
+
+	public String getAmazonApp() {
+
+		return prop.getProperty("appURL_Amazon");
+
+	}
+
+	public String getBrowser() {
+
+		return prop.getProperty("browser");
+
+	}
+
+}
